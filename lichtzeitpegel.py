ac303206ece6b649f42c7e5f357ffefa70a00b07
squawk7x <andreas@ottburg.de>
2022-04-17 21:21:23 +0200

    20220417-21:20 Lichtzeitpegel am Düsseldorfer Rheinturm

---
+++ lichtzeitpegel.py
@@ -0,0 +1,29 @@
+'''
+Lichtzeitpegel am Düsseldorfer Rheinturm
+'''
+
+import time
+
+
+def show_time(delay=1):
+    ''' Dezimaluhr '''
+
+    while True:
+
+        h = divmod(time.localtime().tm_hour, 10)
+        m = divmod(time.localtime().tm_min, 10)
+        s = divmod(time.localtime().tm_sec, 10)
+
+        hour = "{:\u25CB>2s} {:\u25CB>9s}".format(
+            h[0]*'\u25CF', h[1]*'\u25CF')
+        min = "{:\u25CB>5s} {:\u25CB>9s}".format(
+            m[0]*'\u25CF', m[1]*'\u25CF')
+        sec = "{:\u25CB>5s} {:\u25CB>9s}".format(
+            s[0]*'\u25CF', s[1]*'\u25CF')
+
+        print(hour, min, sec)
+        time.sleep(delay)
+
+
+if __name__ == '__main__':
+    show_time()
